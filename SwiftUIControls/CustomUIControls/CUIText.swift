6eed649fec7b49caa09cc8f8a989b2880ff6e6c0
Paulo Silva <silva.pmo@gmail.com>
2021-07-06 23:40:10 +0100

    Add CUIText control code to the project

--- SwiftUIControls/CustomUIControls/CUIText.swift
+++ SwiftUIControls/CustomUIControls/CUIText.swift
@@ -8,13 +8,50 @@
 import SwiftUI
 
 struct CUIText: View {
+    var label: String
+    var text: String
+    var allowCopy: Bool = true
+    
+    init(_ label: String = "", text: String, allowCopy: Bool = true) {
+        self.label = label
+        self.text = text
+        self.allowCopy = allowCopy
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        HStack {
+            if !label.isEmpty {
+                Text(label)
+            }
+            
+            Text(text)
+                .foregroundColor(Color.gray.opacity(0.9))
+            
+            Spacer()
+            
+            if allowCopy {
+                
+                Button(action: {
+                    
+                    // Copy to clipboard
+                    UIPasteboard.general.string = text
+                    
+                }, label: {
+                    Image(systemName: "doc.on.doc.fill" )
+                        .foregroundColor(.blue.opacity(0.55))
+                        .font(.system(size: 20, weight: .bold))
+                        .padding(.leading, 4)
+                })
+                .buttonStyle(BorderlessButtonStyle())
+            }
+            
+        }
     }
 }
 
 struct CUIText_Previews: PreviewProvider {
     static var previews: some View {
-        CUIText()
+        CUIText(text: "Welcome ...")
     }
 }
